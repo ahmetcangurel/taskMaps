850b9556e380842ff7bdeafc07b2932157a2d3b1
ahmetcangurel <cangureliletisim@gmail.com>
2022-10-07 18:12:29 +0300

    final prod

---
+++ src/App.Style.js
@@ -0,0 +1,30 @@
+import { Position } from 'monaco-editor'
+import { StyleSheet } from 'react-native'
+
+export default styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    mapView: {
+        flex: 1
+    },
+    button: {
+        height: 30,
+        backgroundColor: '#33ccff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bottomSheetContainer: {
+        padding: 20,
+    },
+    adressSubTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    adressDescription: {
+        fontSize: 14,
+        fontWeight: '600',
+        marginVertical: 8,
+    },
+})
--- src/App.js
+++ src/App.js
@@ -1,43 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text } from 'react-native'
+import React, { useRef, useState } from 'react';
+import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
+import styles from './App.Style'
 
-import Geolocation from '@react-native-community/geolocation';
-import MapView, { PROVIDER_GOOGLE, Marker, LatLng } from 'react-native-maps';
+import BottomSheet from 'react-native-gesture-bottom-sheet';
+import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 const App = () => {
-  const [coords, setCoords] = useState()
-  const [latitude, setLatitude] = useState()
-  const [longitude, setLongitude] = useState()
+  const [distances, setDistances] = useState('')
+  const [originAdress, setOriginAdress] = useState('')
+  const [destinationAdress, setDestinationAdress] = useState('')
 
-  const getCurrentCoords = () => {
-    Geolocation.getCurrentPosition((res) => {
-      setCoords(res.coords)
-      setLatitude(res.coords.latitude)
-      setLongitude(res.coords.longitude)
-    }),
-      (err) => console.log(err),
-      { enableHighAccuracy: true }
-  }
-
-  useEffect(() => {
-    getCurrentCoords()
-  }, [])
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyDIda-GmIFQuURB9c_iPRwMKOpmVu0xAsM';
+  const bottomSheet = useRef();
 
   const ankaraCoords = {
     latitude: 39.925533,
     longitude: 32.866287,
-    latitudeDelta: 0.1,
-    longitudeDelta: 0.1,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
   };
 
+  const istanbulCoords = {
+    latitude: 41.015137,
+    longitude: 28.979530,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
+  }
+
+  console.log(distances)
+  console.log(originAdress)
+  console.log(destinationAdress)
+
   return (
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      style={{ flex: 1 }}
-      initialRegion={ankaraCoords}>
-      <Marker coordinate={ankaraCoords} />
-    </MapView>
-  );
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => bottomSheet.current.show()}
+        style={styles.button}
+      >
+        <Text>Show Distances</Text>
+      </TouchableOpacity>
+      <BottomSheet hasDraggableIcon ref={bottomSheet} height={600}  >
+        <View style={styles.bottomSheetContainer}>
+          <View>
+            <Text style={styles.adressSubTitle}>Başlangıç Adresiniz:</Text>
+            <Text style={styles.adressDescription}>{originAdress}</Text>
+          </View>
+          <View>
+            <Text style={styles.adressSubTitle}>Varış Adresiniz:</Text>
+            <Text style={styles.adressDescription}>{destinationAdress}</Text>
+          </View>
+          <Text>Mesafeniz: {distances} KM</Text>
+        </View>
+      </BottomSheet>
+      <MapView
+        style={styles.mapView}
+        initialRegion={ankaraCoords}>
+        <Marker
+          coordinate={ankaraCoords}
+          pinColor='#33ccff'
+        />
+        <Marker
+          coordinate={istanbulCoords}
+          pinColor='#e3e3e3'
+        />
+        <MapViewDirections
+          origin={ankaraCoords}
+          destination={istanbulCoords}
+          apikey={GOOGLE_MAPS_APIKEY}
+          language='tr'
+          mode='DRIVING'
+          strokeWidth={4}
+          strokeColor='hotpink'
+          onReady={res => {
+            setDistances(res.distance)
+            setOriginAdress(res.legs[0].start_address)
+            setDestinationAdress(res.legs[0].end_address)
+          }}
+        />
+      </MapView >
+    </View>
+  )
 }
 
 export default App
