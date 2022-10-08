71460e71059ca6ed68315a77e999a71affb3a685
ahmetcangurel <cangureliletisim@gmail.com>
2022-10-08 20:08:09 +0300

    maps

--- src/App.js
+++ src/App.js
@@ -7,30 +7,47 @@ import MapView, { Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 
 const App = () => {
-  const [distances, setDistances] = useState('')
-  const [originAdress, setOriginAdress] = useState('')
-  const [destinationAdress, setDestinationAdress] = useState('')
-
   const GOOGLE_MAPS_APIKEY = 'AIzaSyDIda-GmIFQuURB9c_iPRwMKOpmVu0xAsM';
   const bottomSheet = useRef();
+  const [adressList, setAdressList] = useState([])
+  const [adressItem, setAdressItem] = useState([])
 
   const ankaraCoords = {
     latitude: 39.925533,
     longitude: 32.866287,
     latitudeDelta: 10,
     longitudeDelta: 10,
-  };
-
-  const istanbulCoords = {
-    latitude: 41.015137,
-    longitude: 28.979530,
-    latitudeDelta: 10,
-    longitudeDelta: 10,
   }
 
-  console.log(distances)
-  console.log(originAdress)
-  console.log(destinationAdress)
+  const cityCoords = [
+    {
+      cityName: 'İstanbul',
+      coord: {
+        latitude: 41.015137,
+        longitude: 28.979530,
+        latitudeDelta: 10,
+        longitudeDelta: 10,
+      }
+    },
+    {
+      cityName: 'Bursa',
+      coord: {
+        latitude: 40.193298,
+        longitude: 29.074202,
+        latitudeDelta: 10,
+        longitudeDelta: 10,
+      }
+    },
+    {
+      cityName: 'Samsun',
+      coord: {
+        latitude: 41.279703,
+        longitude: 36.336067,
+        latitudeDelta: 10,
+        longitudeDelta: 10,
+      }
+    }
+  ]
 
   return (
     <View style={styles.container}>
@@ -44,13 +61,13 @@ const App = () => {
         <View style={styles.bottomSheetContainer}>
           <View>
             <Text style={styles.adressSubTitle}>Başlangıç Adresiniz:</Text>
-            <Text style={styles.adressDescription}>{originAdress}</Text>
+            <Text style={styles.adressDescription}>{adressItem.originAdress} </Text>
           </View>
           <View>
             <Text style={styles.adressSubTitle}>Varış Adresiniz:</Text>
-            <Text style={styles.adressDescription}>{destinationAdress}</Text>
+            <Text style={styles.adressDescription}>{adressItem.destinationAdress}</Text>
           </View>
-          <Text>Mesafeniz: {distances} KM</Text>
+          <Text>Mesafeniz: {adressItem.distances} KM</Text>
         </View>
       </BottomSheet>
       <MapView
@@ -60,24 +77,34 @@ const App = () => {
           coordinate={ankaraCoords}
           pinColor='#33ccff'
         />
-        <Marker
-          coordinate={istanbulCoords}
-          pinColor='#e3e3e3'
-        />
-        <MapViewDirections
-          origin={ankaraCoords}
-          destination={istanbulCoords}
-          apikey={GOOGLE_MAPS_APIKEY}
-          language='tr'
-          mode='DRIVING'
-          strokeWidth={4}
-          strokeColor='hotpink'
-          onReady={res => {
-            setDistances(res.distance)
-            setOriginAdress(res.legs[0].start_address)
-            setDestinationAdress(res.legs[0].end_address)
-          }}
-        />
+        {cityCoords.map((city) => {
+          return (
+            <>
+              <Marker
+                key={city.cityName}
+                coordinate={city.coord}
+              />
+              <MapViewDirections
+                key={city}
+                origin={ankaraCoords}
+                destination={city.coord}
+                apikey={GOOGLE_MAPS_APIKEY}
+                language='tr'
+                mode='DRIVING'
+                strokeWidth={4}
+                strokeColor='hotpink'
+                onReady={(res => {
+                  setAdressItem(...adressItem,{
+                    distances: res.distance,
+                    originAdress: res.legs[0].start_address,
+                    destinationAdress: res.legs[0].end_address
+                  })
+                })}
+              />
+            </>
+          )
+        })}
+
       </MapView >
     </View>
   )
